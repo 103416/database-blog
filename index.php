a2774d6ec5571ee8baab58719c71d21ca0fb3ea8
103416 <103416@glr.nl>
2026-09-08 15:26:08 +0200

    style joepie

--- index.php
+++ index.php
@@ -32,10 +32,12 @@ $result = $stmt->fetchAll();
 
 <h3>"Wat als ik niet gewelkomt wil worden"</h3>
     <?php foreach ($result as $row) { ?>
-<!--    <a href="" class="ding"></a>-->
-         <h1><?= $row['BlogOnderwerp']?></h1>
-        <h3><?= $row["BlogSubTekst"]?></h3>
-        <p><?= $row["BlogBody"]?></p>
+        <div class="db_section">
+<!--        <a href="" class="ding"></a>-->
+            <h1><?= $row['BlogOnderwerp']?></h1>
+            <h3><?= $row["BlogSubTekst"]?></h3>
+            <p><?= $row["BlogBody"]?></p>
+        </div>
     <?php }?>
 </body>
 </html>
