0672bd60aa1e482a050d2e1f40cd8e332ee5d596
103421 <103421@glr.nl>
2026-09-10 10:55:22 +0200

    grote update, styling isch

--- Delete/index.php
+++ Delete/index.php
@@ -21,6 +21,7 @@ $result = $stmt->fetchAll();
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="../style/tables.css">
 </head>
 <body>
     <table border="1px">
