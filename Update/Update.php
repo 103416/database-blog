832dd3ad20dcafb3d7dceac69d6d2792ecb50e73
103422 <103422@glr.nl>
2026-09-11 09:53:50 +0200

    Styling change: Update.php -changed button color -changed styling and background of page

--- Update/Update.php
+++ Update/Update.php
@@ -21,30 +21,38 @@ $results = $stmt->fetchAll();
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../style/main.css">
+    <link rel="stylesheet" href="../style/form.css">
     <title>Document</title>
 </head>
 <body>
 <h1></h1>
 <?php foreach ($results as $result) { ?>
 <form method="post" action="./verwerking_update.php?ID=<?= $result['BlogNummer']; ?>"">
-    <label for="onderwerp">je onderwerp:</label>
+    <label for="onderwerp">Je onderwerp:</label>
     <input type="text" id="onderwerp" name="onderwerp" value="<?= $result['BlogOnderwerp'] ?>">
 
-    <label for="subtekst">subtekst</label>
+    <label for="subtekst">Subtekst</label>
     <input type="text" id="subtekst" name="subtekst" value="<?= $result['BlogSubTekst'] ?>">
 
-    <label for="body">body tekst</label>
+    <label for="body">Body tekst</label>
     <input type="text" id="body" name="body"value="<?php echo $result['BlogBody'] ?>">
 
 
 
-    <label for="wie">wie</label>
-    <select name="Wie" id="wie" required>
+    <label for="wie">Wie</label>
+    <select name="Wie" id="wie" Required>
         <option value="Ruben">Ruben</option>
         <option value="Sven">Sven</option>
         <option value="Niels">Niels</option>
     </select>
-    <input type="submit">
+
+    <input id="submit-button" type="submit">
+    <style>
+        #submit-button{
+            background-color: #b798ff;
+        }
+    </style>
 </form>
 <?php } ?>
 </body>
